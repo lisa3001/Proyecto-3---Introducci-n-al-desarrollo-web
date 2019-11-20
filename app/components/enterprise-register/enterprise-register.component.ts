1825127b768b3958dcbbd629311cdb2716b2c0f4
Luis Fernando Molina Juarez <Luismj138@hotmail.com>
2019-11-20 11:31:52 -0600

    Componente EnterpriseRegister agregado

---
+++ app/components/enterprise-register/enterprise-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enterprise-register',
+  templateUrl: './enterprise-register.component.html',
+  styleUrls: ['./enterprise-register.component.scss']
+})
+export class EnterpriseRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
